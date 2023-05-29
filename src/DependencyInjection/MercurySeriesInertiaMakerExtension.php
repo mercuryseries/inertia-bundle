04bd58501f0beca72325ff372628e09217001c4f
Honore Hounwanou <mercuryseries@gmail.com>
2023-05-29 03:00:40 -0400

    WIP

--- src/DependencyInjection/MercurySeriesInertiaMakerExtension.php
+++ src/DependencyInjection/MercurySeriesInertiaMakerExtension.php
@@ -10,6 +10,25 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class MercurySeriesInertiaMakerExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        $configs = $container->getExtensionConfig($this->getAlias());
+
+        // iterate in reverse to preserve the original order after prepending the config
+        foreach (array_reverse($configs) as $config) {
+            // check if ssr is set in the "mercuryseries_inertia_maker" configuration
+            if (isset($config['ssr'])) {
+                // prepend the rompetomp_inertia settings with the ssr
+                $container->prependExtensionConfig('rompetomp_inertia', [
+                    'ssr' => [
+                        'enabled' => $config['ssr']['enabled'],
+                        'url' => $config['ssr']['url']
+                    ]
+                ]);
+            }
+        }
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
@@ -31,11 +50,6 @@ final class MercurySeriesInertiaMakerExtension extends Extension implements Prep
         // $definition = new Definition(ExceptionListener::class);
         // $definition->addTag('kernel.event_listener', ['event' => 'kernel.exception']);
         // $container->setDefinition(ExceptionListener::class, $definition);
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        // ...
     }
 
     public function getAlias(): string
